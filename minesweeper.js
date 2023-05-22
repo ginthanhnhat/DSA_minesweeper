ce3ab2629d1c0998cc241b34dd3777701da244d8
ITITIU20337 - NGUYỄN HUỲNH ANH TÚ <91893958+ITITIU20337@users.noreply.github.com>
2023-05-22 21:45:48 +0700

    Update function startGame
    Add event listener to flag ( flag 1 2 and 3 )

--- minesweeper.js
+++ minesweeper.js
@@ -5,4 +5,7 @@ function startGame() {
     document.getElementById("bomb1").addEventListener("click", setBomb1);
     document.getElementById("bomb2").addEventListener("click", setBomb2);
     document.getElementById("bomb3").addEventListener("click", setBomb3);
-  
+    document.getElementById("flag1").addEventListener("click", setflag1);
+    document.getElementById("flag2").addEventListener("click", setflag2);
+    document.getElementById("flag3").addEventListener("click", setflag3);
+}    
