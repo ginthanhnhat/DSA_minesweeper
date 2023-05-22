c8b25ba04f8fa35316cf418bc4d4489f235a0782
ITITIU20337 - NGUYỄN HUỲNH ANH TÚ <91893958+ITITIU20337@users.noreply.github.com>
2023-05-22 21:38:45 +0700

    Update startGame function
    Add event listener to choose one in  three different type of bomb

--- minesweeper.js
+++ minesweeper.js
@@ -1,3 +1,8 @@
 window.onload = function () {
     startGame();
 }
+function startGame() {
+    document.getElementById("bomb1").addEventListener("click", setBomb1);
+    document.getElementById("bomb2").addEventListener("click", setBomb2);
+    document.getElementById("bomb3").addEventListener("click", setBomb3);
+  
