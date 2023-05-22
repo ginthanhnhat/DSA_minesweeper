f4b2a6a1b3d681c75086f24fd9dacf11b4c2e005
ITITIU20337 - NGUYỄN HUỲNH ANH TÚ <91893958+ITITIU20337@users.noreply.github.com>
2023-05-22 22:02:19 +0700

    Update startGame()
    Default number of bomb in the board

--- minesweeper.js
+++ minesweeper.js
@@ -8,4 +8,5 @@ function startGame() {
     document.getElementById("flag1").addEventListener("click", setflag1);
     document.getElementById("flag2").addEventListener("click", setflag2);
     document.getElementById("flag3").addEventListener("click", setflag3);
+    document.getElementById("mines-count").innerText = minesCount;
 }    
