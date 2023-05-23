41d1d3be7fa71be5fc000404151524aa62403345
ITITIU20337 - NGUYỄN HUỲNH ANH TÚ <91893958+ITITIU20337@users.noreply.github.com>
2023-05-23 11:59:43 +0700

    Update setMines()
    Randomly set mines in the board

--- minesweeper.js
+++ minesweeper.js
@@ -13,18 +13,34 @@ function startGame() {
     document.getElementById("flag2").addEventListener("click", setflag2);
     document.getElementById("flag3").addEventListener("click", setflag3);
     document.getElementById("mines-count").innerText = minesCount;
-    document.getElementById("flag-button").innerText = flag;   
+    document.getElementById("flag-button").innerText = flag;
+    setMines();   
 
-for (let r = 0; r < rows; r++) {
-    let row = [];
-    for (let c = 0; c < columns; c++) {
-      let tile = document.createElement("div");
-      tile.id = r.toString() + "-" + c.toString();
-      tile.addEventListener("click", clickTile);    
-      tile.addEventListener("contextmenu", putFlag);   
-      document.getElementById("board").append(tile);
-      row.push(tile);
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+        let tile = document.createElement("div");
+        tile.id = r.toString() + "-" + c.toString();
+        tile.addEventListener("click", clickTile);    
+        tile.addEventListener("contextmenu", putFlag);   
+        document.getElementById("board").append(tile);
+        row.push(tile);
+        }
+        board.push(row);
     }
-    board.push(row);
 }
+function setMines() {
+    let minesLeft = minesCount;
+    while (minesLeft > 0) {
+      let r = Math.floor(Math.random() * rows);
+      let c = Math.floor(Math.random() * columns);
+      let id = r.toString() + "-" + c.toString();
+  
+  
+      if (!minesLocation.includes(id)) {
+        minesLocation.push(id);
+        minesLeft -= 1;
+      }
+    }
 }
+  
